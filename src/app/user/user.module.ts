a505eca8f617544c7b4fb5b91a0869a3e71c1f39
MoizAnsari7 <moizansari176@gmail.com>
2024-11-11 18:42:36 +0530

    Cart Component Added

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -9,8 +9,8 @@ import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
-    ProfileComponent
-    CartComponent,
+    ProfileComponent,
+    CartComponent
   ],
   imports: [
     CommonModule,
